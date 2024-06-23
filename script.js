e7b1ce51202e724b29ace14b03b45f162e648ca8
tomstoten <tomstoughton1@gmail.com>
2024-06-23 12:15:54 -0600

    working drag and drop with nested dropzones

---
+++ script.js
@@ -0,0 +1,95 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var ElementNotFoundError = /** @class */ (function (_super) {
+    __extends(ElementNotFoundError, _super);
+    function ElementNotFoundError(elementId) {
+        var _this = _super.call(this, "Element with ID '".concat(elementId, "' not found")) || this;
+        _this.name = "ElementNotFoundError";
+        return _this;
+    }
+    return ElementNotFoundError;
+}(Error));
+function modifyID(parent) {
+    var id = parent.id;
+    var cloneCount = Number(parent.getAttribute('data-numclones'));
+    parent.setAttribute('data-numclones', String(cloneCount + 1));
+    var newID = id + "-clone" + String(cloneCount + 1);
+    return newID;
+}
+function onDragStart(event) {
+    event
+        .dataTransfer
+        .setData('text/plain', event.target.id);
+    var id = event
+        .dataTransfer
+        .getData('text');
+    var element = document.getElementById(id);
+    if (!element)
+        throw new ElementNotFoundError(id);
+    if (element.getAttribute('data-cloneondrag') == 'true') {
+        event.currentTarget.style.backgroundColor = 'yellow';
+    }
+    else {
+        event.currentTarget.style.backgroundColor = 'red';
+    }
+}
+function onDragEnd(event) {
+    var id = event
+        .dataTransfer
+        .getData('text');
+    var element = document.getElementById(id);
+    event.currentTarget.style.backgroundColor = '#4AAE9B';
+}
+function onDragOver(event) {
+    event.preventDefault();
+}
+function onDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var id = event
+        .dataTransfer
+        .getData('text');
+    var draggableElement = document.getElementById(id);
+    var dropZone = event.target;
+    var trash = dropZone.getAttribute("class") == "trash-dropzone";
+    console.log("DropZone:");
+    console.log(dropZone);
+    if (!draggableElement)
+        throw new ElementNotFoundError(id);
+    if (draggableElement.getAttribute('data-cloneondrag') == 'true') // root element
+     {
+        if (!trash) {
+            var copyElement = draggableElement.cloneNode(true);
+            copyElement.setAttribute('data-cloneondrag', 'false');
+            copyElement.setAttribute('id', modifyID(draggableElement));
+            dropZone.appendChild(copyElement);
+        }
+    }
+    else {
+        if (trash) {
+            var parent_1 = draggableElement.parentElement;
+            if (!parent_1)
+                return;
+            parent_1.removeChild(draggableElement);
+        }
+        else
+            dropZone.appendChild(draggableElement);
+    }
+    console.log(draggableElement);
+    event
+        .dataTransfer
+        .clearData();
+}
---
+++ script.ts
@@ -0,0 +1,98 @@
+class ElementNotFoundError extends Error
+{
+    constructor(elementId: string) {
+        super(`Element with ID '${elementId}' not found`);
+        this.name = "ElementNotFoundError";
+    }
+}
+
+function modifyID(parent : HTMLElement) {
+    const id : string = parent.id;
+    const cloneCount : number = Number(parent.getAttribute('data-numclones'));
+    parent.setAttribute('data-numclones', String(cloneCount + 1));
+    
+    var newID : string = id + "-clone" + String(cloneCount + 1);
+    return newID;
+}
+
+function onDragStart(event) {
+    event
+        .dataTransfer
+        .setData('text/plain', event.target.id);
+    
+    const id = event
+        .dataTransfer
+        .getData('text');
+    
+    const element = document.getElementById(id);
+
+    if (!element) throw new ElementNotFoundError(id);
+    if (element.getAttribute('data-cloneondrag')=='true') 
+    {
+        event.currentTarget.style.backgroundColor = 'yellow';
+    }
+    else
+    {
+        event.currentTarget.style.backgroundColor = 'red';
+    }
+    
+}
+
+function onDragEnd(event) {
+    const id = event
+        .dataTransfer
+        .getData('text');
+    
+    const element = document.getElementById(id);
+    event.currentTarget.style.backgroundColor = '#4AAE9B';
+}
+
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const id = event
+      .dataTransfer
+      .getData('text');
+
+    const draggableElement = document.getElementById(id);
+
+    const dropZone = event.target as HTMLElement;
+    const trash = dropZone.getAttribute("class")=="trash-dropzone";
+
+    console.log("DropZone:");
+    console.log(dropZone);
+
+    if (!draggableElement) throw new ElementNotFoundError(id);
+    if (draggableElement.getAttribute('data-cloneondrag')=='true') // root element
+    {
+        if(!trash)
+        {
+            const copyElement = draggableElement.cloneNode(true) as HTMLElement;
+            copyElement.setAttribute('data-cloneondrag', 'false');
+            copyElement.setAttribute('id', modifyID(draggableElement))
+            dropZone.appendChild(copyElement);
+        }
+    }
+    else
+    {
+        if(trash)
+        {
+            const parent = draggableElement.parentElement;
+            if (!parent) return;
+            parent.removeChild(draggableElement);
+        }
+        else
+            dropZone.appendChild(draggableElement);
+    }
+
+    console.log(draggableElement);
+    
+    event
+        .dataTransfer
+        .clearData();
+  }
